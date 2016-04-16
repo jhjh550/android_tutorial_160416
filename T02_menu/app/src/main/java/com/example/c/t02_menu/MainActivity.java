62d0614b4e0a4ac9f90623a0cb881920109e22eb
jh park <jhjh550@gmail.com>
2016-04-16 17:21:17 +0900

    - menu

--- T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
+++ T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.c.t02_menu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_test){
+            Toast.makeText(this, "menu selected", Toast.LENGTH_LONG).show();
+        }
+        return  true;
     }
 }
+
+
+

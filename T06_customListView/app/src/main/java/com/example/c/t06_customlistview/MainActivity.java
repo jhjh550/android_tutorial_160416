b5f4e8f83277dbe0e3953dd81315f17a246f242c
jh park <jhjh550@gmail.com>
2016-04-17 15:51:53 +0900

    - listview

--- T06_customListView/app/src/main/java/com/example/c/t06_customlistview/MainActivity.java
+++ T06_customListView/app/src/main/java/com/example/c/t06_customlistview/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.c.t06_customlistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,6 +24,43 @@ public class MainActivity extends AppCompatActivity {
             this.title = title;
             this.desc = desc;
             this.imgIcon = imgIcon;
+        }
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                LayoutInflater inf = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                convertView = inf.inflate(R.layout.item_view, null);
+            }
+
+            TextView textViewTitle = (TextView)convertView.findViewById(R.id.textViewTitle);
+            TextView textViewDesc = (TextView)convertView.findViewById(R.id.textViewDesc);
+            ImageView item_icon = (ImageView)convertView.findViewById(R.id.item_icon);
+
+            MyData data = list.get(position);
+            textViewTitle.setText(data.title);
+            textViewDesc.setText(data.desc);
+            item_icon.setImageResource(data.imgIcon);
+
+            return convertView;
         }
     }
 

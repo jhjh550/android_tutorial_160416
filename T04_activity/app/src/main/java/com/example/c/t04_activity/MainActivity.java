143bcb303ea3c8db58496f4f8b808364284e4fb8
jh park <jhjh550@gmail.com>
2016-04-17 13:43:03 +0900

    -activity

--- T04_activity/app/src/main/java/com/example/c/t04_activity/MainActivity.java
+++ T04_activity/app/src/main/java/com/example/c/t04_activity/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 intent.putExtra("myId", "abcd");
-                startActivity(intent);
+                startActivityForResult(intent);
             }
         });
     }

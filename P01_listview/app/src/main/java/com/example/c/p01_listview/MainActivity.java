f5872e1a7d5e62087b7a31d00730dd1c7ddd23f0
jh park <jhjh550@gmail.com>
2016-04-17 18:52:36 +0900

    p

--- P01_listview/app/src/main/java/com/example/c/p01_listview/MainActivity.java
+++ P01_listview/app/src/main/java/com/example/c/p01_listview/MainActivity.java
@@ -4,14 +4,17 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] strArrays;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +28,26 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i<files.length; i++){
             File f = files[i];
             if(f.isDirectory() == false) {
-                String name =f.getName();
+                String name = f.getName();
                 int index = name.lastIndexOf(".wav");
                 if(index>0)
-                    list.add(f.getName());
+                    list.add(name);
             }
         }
 
-        String[] strArrays = list.toArray(new String[list.size()]);
+        strArrays = list.toArray(new String[list.size()]);
 
         ListView listView = (ListView)findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, strArrays);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,strArrays[position],Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }

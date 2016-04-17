5e07d02526a89c20a3889f8c785226fe66ccd639
jh park <jhjh550@gmail.com>
2016-04-17 12:33:35 +0900

    adaf

--- T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
+++ T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
@@ -1,7 +1,12 @@
+
+
 package com.example.c.t03_widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox myCheck = (CheckBox) findViewById(R.id.checkbox);
+        myCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "CheckBox change : "+isChecked, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

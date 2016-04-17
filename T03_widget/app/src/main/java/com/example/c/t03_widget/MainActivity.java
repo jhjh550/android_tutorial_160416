009f080f47a69ac4f5295aa19d6e284d36cfe1e9
jh park <jhjh550@gmail.com>
2016-04-17 13:20:01 +0900

    -activity

--- T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
+++ T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
@@ -4,6 +4,8 @@ package com.example.c.t03_widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -43,8 +45,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        EditText editText = (EditText)findViewById(R.id.editText);
-
+        final EditText editText = (EditText)findViewById(R.id.editText);
+        Button btnOK = (Button)findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "text : "+str, Toast.LENGTH_LONG).show();
+                editText.setText("");
+            }
+        });
     }
 }
 

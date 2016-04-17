0e4426790247d6a391be6451ef2e127f5434493d
jh park <jhjh550@gmail.com>
2016-04-17 19:20:49 +0900

    p

--- P01_listview/app/src/main/java/com/example/c/p01_listview/MyActivity.java
+++ P01_listview/app/src/main/java/com/example/c/p01_listview/MyActivity.java
@@ -1,13 +1,53 @@
 package com.example.c.p01_listview;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MyActivity extends AppCompatActivity {
 
+    MediaPlayer mp = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Intent intent = getIntent();
+        String fileName = intent.getStringExtra("fileName");
+
+        final String path = Environment.getExternalStorageDirectory()+"/Music/"+fileName;
+
+        Button btnPlay = (Button)findViewById(R.id.btnPlay);
+        Button btnStop = (Button)findViewById(R.id.btnStop);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp = new MediaPlayer();
+                try {
+                    mp.setDataSource(path);
+                    mp.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mp.start();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp!=null){
+                    mp.stop();
+                    mp.release();
+                    mp = null;
+                }
+            }
+        });
     }
 }

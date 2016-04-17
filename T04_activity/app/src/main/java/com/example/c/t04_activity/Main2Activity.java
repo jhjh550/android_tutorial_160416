f990557a64fa887e39c0c39a46e0e9173c67cfd6
jh park <jhjh550@gmail.com>
2016-04-17 13:40:16 +0900

    -activity

--- T04_activity/app/src/main/java/com/example/c/t04_activity/Main2Activity.java
+++ T04_activity/app/src/main/java/com/example/c/t04_activity/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.c.t04_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("myId");
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
     }
 }
--- T04_activity/app/src/main/java/com/example/c/t04_activity/MainActivity.java
+++ T04_activity/app/src/main/java/com/example/c/t04_activity/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("myId", "abcd");
                 startActivity(intent);
             }
         });

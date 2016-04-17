a7f968cd04d2e14b1791df2a6a41692e408b99ab
jh park <jhjh550@gmail.com>
2016-04-17 14:45:29 +0900

    -listview

--- T05_listview/app/src/main/java/com/example/c/t05_listview/MainActivity.java
+++ T05_listview/app/src/main/java/com/example/c/t05_listview/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.c.t05_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] list = {"hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
     }
 }

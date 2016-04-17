c022fbe93b57d7d847c1217c3343d285b57a2d2e
jh park <jhjh550@gmail.com>
2016-04-17 15:09:04 +0900

    - listview

--- T05_listview/app/src/main/java/com/example/c/t05_listview/MainActivity.java
+++ T05_listview/app/src/main/java/com/example/c/t05_listview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.c.t05_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = list[position];
+                Toast.makeText(MainActivity.this, "str : "+str, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ T06_customListView/app/src/main/java/com/example/c/t06_customlistview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.c.t06_customlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    class MyData{
+        String title;
+        String desc;
+        int imgIcon;
+
+        public MyData(String title, String desc, int imgIcon) {
+            this.title = title;
+            this.desc = desc;
+            this.imgIcon = imgIcon;
+        }
+    }
+
+    ArrayList<MyData> list = new ArrayList<MyData>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int icon = 0;
+        for(int i=0; i<30; i++){
+            switch (i%4) {
+                case 0:
+                    icon = android.R.drawable.ic_btn_speak_now;
+                    break;
+                case 1:
+                    icon = android.R.drawable.ic_delete;
+                    break;
+                case 2:
+                    icon = android.R.drawable.ic_media_next;
+                    break;
+                case 3:
+                    icon = android.R.drawable.ic_media_ff;
+                    break;
+            }
+
+            MyData data = new MyData("data"+i, "desc"+i, icon);
+            list.add(data);
+        }
+
+    }
+}

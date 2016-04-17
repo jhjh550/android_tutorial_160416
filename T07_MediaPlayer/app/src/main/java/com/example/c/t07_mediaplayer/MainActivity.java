c0dbfea5b6f7120d74f4fd15ed0b265acacf97d3
jh park <jhjh550@gmail.com>
2016-04-17 16:44:50 +0900

    -mp

--- T07_MediaPlayer/app/src/main/java/com/example/c/t07_mediaplayer/MainActivity.java
+++ T07_MediaPlayer/app/src/main/java/com/example/c/t07_mediaplayer/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.c.t07_mediaplayer;
 
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mp = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPlay = (Button)findViewById(R.id.btnPlay);
+        Button btnStop = (Button)findViewById(R.id.btnStop);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = Environment.getExternalStorageDirectory()+"/Music/MyStory.wav";
+                mp = new MediaPlayer();
+                try {
+                    mp.setDataSource(path);
+                    mp.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mp.start();
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mp != null){
+                    mp.stop();
+                    mp.release();
+                    mp = null;
+                }
+            }
+        });
     }
 }

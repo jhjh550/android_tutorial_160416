ab8b37d4c6c32b77248059daf2d25ff41fbee754
jh park <jhjh550@gmail.com>
2016-04-23 11:31:45 +0900

    -thread

--- T10_thread/app/src/main/java/com/example/c/t10_thread/MainActivity.java
+++ T10_thread/app/src/main/java/com/example/c/t10_thread/MainActivity.java
@@ -1,23 +1,70 @@
 package com.example.c.t10_thread;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LOG_COUNT = 1;
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(msg.what == LOG_COUNT){
+                textView.setText("count : "+msg.arg1);
+            }
+        }
+    };
+
+    class MyThread extends Thread{
+        public void run(){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //
+        }
+    };
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread th = new Thread(new Runnable() {
+        textView = (TextView) findViewById(R.id.textView);
+        Runnable r = new Runnable() {
             @Override
             public void run() {
                 for(int i=0; i<100; i++){
-                    Thread.sleep(100);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d("thread","count : "+i);
+
+                    Message msg = new Message();
+                    msg.what = LOG_COUNT;
+                    msg.arg1 = i;
+
+                    handler.sendMessage(msg);
+
                 }
             }
-        });
+        };
+        Thread th = new Thread(r);
         th.start();
+
+//        Thread th2 = new Thread(r);
+//        th2.start();
+//
+//        MyThread th3 = new MyThread();
+//        th3.start();
     }
 }

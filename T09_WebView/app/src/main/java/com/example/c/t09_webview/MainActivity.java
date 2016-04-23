cebc29430d93eae5db3250a4a65994b76c50b4f6
jh park <jhjh550@gmail.com>
2016-04-23 09:59:07 +0900

    -webview

--- T09_WebView/app/src/main/java/com/example/c/t09_webview/MainActivity.java
+++ T09_WebView/app/src/main/java/com/example/c/t09_webview/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.c.t09_webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WebView webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.naver.com");
+
+        Button btn = (Button) findViewById(R.id.btnLoad);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editUrl = (EditText) findViewById(R.id.editURL);
+                String str = editUrl.getText().toString();
+                webView.loadUrl(str);
+            }
+        });
     }
 }

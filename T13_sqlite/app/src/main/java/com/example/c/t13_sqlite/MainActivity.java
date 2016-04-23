166417d7713d7855064f585e56d8e38fc6ecab7c
jh park <jhjh550@gmail.com>
2016-04-23 15:55:42 +0900

    sqlite

--- T13_sqlite/app/src/main/java/com/example/c/t13_sqlite/MainActivity.java
+++ T13_sqlite/app/src/main/java/com/example/c/t13_sqlite/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.c.t13_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        TestSQLiteHandler dbHandler = new TestSQLiteHandler(this);
+        dbHandler.insert("kim", 20, "서울");
+        dbHandler.insert("lee", 30, "인천");
+
+        dbHandler.update("kim", 22);
+
+        String res = dbHandler.selectAll();
+        textView.setText(res);
+
+
     }
 }

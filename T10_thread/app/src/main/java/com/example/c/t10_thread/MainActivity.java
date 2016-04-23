303ce15f54f862591527e83e26e6a062bb1742b3
jh park <jhjh550@gmail.com>
2016-04-23 12:07:11 +0900

    -thread

--- T10_thread/app/src/main/java/com/example/c/t10_thread/MainActivity.java
+++ T10_thread/app/src/main/java/com/example/c/t10_thread/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
             if(msg.what == LOG_COUNT){
                 textView.setText("count : "+msg.arg1);
+                progressBar.setProgress(msg.arg1);
             }
         }
     };
@@ -32,11 +34,13 @@ public class MainActivity extends AppCompatActivity {
         }
     };
     TextView textView;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
         textView = (TextView) findViewById(R.id.textView);
         Runnable r = new Runnable() {
             @Override

fb4db68cd5e5662cffb4246c5715275d8994ae6c
jh park <jhjh550@gmail.com>
2016-04-23 18:04:36 +0900

    -xml

--- T14_xml/app/src/main/java/com/example/c/t14_xml/MainActivity.java
+++ T14_xml/app/src/main/java/com/example/c/t14_xml/MainActivity.java
@@ -1,19 +1,8 @@
 package com.example.c.t14_xml;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
@@ -24,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
 
-        MyDomParser task = new MyDomParser(textView);
+        //MyDomParser task = new MyDomParser(textView);
+        MyPullParser task = new MyPullParser(textView);
         task.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153054000");
     }
 }

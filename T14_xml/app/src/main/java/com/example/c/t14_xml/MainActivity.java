cdf523a10952327bac1a7e4c0a4b5e2b383eee06
jh park <jhjh550@gmail.com>
2016-04-23 17:02:16 +0900

    -xml

--- T14_xml/app/src/main/java/com/example/c/t14_xml/MainActivity.java
+++ T14_xml/app/src/main/java/com/example/c/t14_xml/MainActivity.java
@@ -5,19 +5,60 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     class MyDomParser extends AsyncTask<String, Void, Document>{
 
+        private String getElementText(Element dataElement, String tag){
+            NodeList dayNodeList = dataElement.getElementsByTagName(tag);
+            Element dayElement = (Element) dayNodeList.item(0);
+
+            NodeList textNodeList = dayElement.getChildNodes();
+            String val = textNodeList.item(0).getNodeValue();
+
+            return val;
+        }
+        @Override
+        protected void onPostExecute(Document document) {
+            super.onPostExecute(document);
+            String res = "";
+            NodeList nodeList = document.getElementsByTagName("data");
+            for(int i=0; i<nodeList.getLength(); i++){
+                Element dataElement = (Element) nodeList.item(i);
+                String day = getElementText(dataElement, "day");
+            }
+        }
+
         @Override
         protected Document doInBackground(String... params) {
-            return null;
+            URL url = null;
+            Document document = null;
+            try {
+                url = new URL(params[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = dbf.newDocumentBuilder();
+                document = builder.parse(url.openStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return document;
         }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyDomParser task = new MyDomParser();
+        task.execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153054000");
     }
 }

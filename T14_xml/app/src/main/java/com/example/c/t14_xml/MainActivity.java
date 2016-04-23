ef6d8252e89748517908271281d4056319f1bf1e
jh park <jhjh550@gmail.com>
2016-04-23 16:18:04 +0900

    -xml

--- T14_xml/app/src/main/java/com/example/c/t14_xml/MainActivity.java
+++ T14_xml/app/src/main/java/com/example/c/t14_xml/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.c.t14_xml;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.w3c.dom.Document;
+
 public class MainActivity extends AppCompatActivity {
 
+    class MyDomParser extends AsyncTask<String, Void, Document>{
+
+        @Override
+        protected Document doInBackground(String... params) {
+            return null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

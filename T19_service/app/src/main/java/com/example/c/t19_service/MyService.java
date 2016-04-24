e0aa645555f89a92275c6f2d290ff6451762811f
jh park <jhjh550@gmail.com>
2016-04-24 13:51:11 +0900

    -service

--- T19_service/app/src/main/java/com/example/c/t19_service/MyService.java
+++ T19_service/app/src/main/java/com/example/c/t19_service/MyService.java
@@ -3,9 +3,28 @@ package com.example.c.t19_service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("service", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("service", "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("service", "onDestroy");
     }
 
     @Override

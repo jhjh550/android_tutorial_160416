36111c635f920f2542a1a6b8998c818cd0ca4466
jh park <jhjh550@gmail.com>
2016-04-24 16:17:55 +0900

    -style

--- T21_style/app/src/main/java/com/example/c/t21_style/MainActivity.java
+++ T21_style/app/src/main/java/com/example/c/t21_style/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.c.t21_style;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView selectedText, workingTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectedText = (TextView)findViewById(R.id.selectedTextView);
+        workingTextView = (TextView)findViewById(R.id.workingTextView);
+
+        View.OnClickListener numberListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button btn = (Button)v;
+                String value = btn.getText().toString();
+                String working = workingTextView.getText().toString();
+                if(working.equals("0")){
+                    workingTextView.setText(value);
+                }else{
+                    workingTextView.setText(working+value);
+                }
+            }
+        };
+        Button zeroBtn = (Button)findViewById(R.id.zeroBtn);
+        Button oneBtn = (Button)findViewById(R.id.oneBtn);
+        zeroBtn.setOnClickListener(numberListener);
+        oneBtn.setOnClickListener(numberListener);
+
     }
 }

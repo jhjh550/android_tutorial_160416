f288663a47ad0e988fde45cdc22321fe7c050d49
jh park <jhjh550@gmail.com>
2016-04-30 16:16:53 +0900

    - noti

--- T25_notification/app/src/main/java/com/example/c/t25_notification/MainActivity.java
+++ T25_notification/app/src/main/java/com/example/c/t25_notification/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.c.t25_notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
@@ -19,8 +23,18 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager manger =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Intent intent = new Intent(this, NotiActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pIntent = PendingIntent.getActivity(this,0, intent, 0);
 
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.tulips);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("TITLE");
+        builder.setContentText("TEXT");
+        builder.setSubText("SUB TEXT");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(bm);
+        builder.setContentIntent(pIntent);
         Notification noti = builder.build();
 
         manger.notify(1234, noti);

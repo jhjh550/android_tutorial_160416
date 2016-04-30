ff3349a32c8d5987c58cffecaa3866b721d618a2
jh park <jhjh550@gmail.com>
2016-04-30 14:24:40 +0900

    -fragment2

--- T24_fragment2/app/src/main/java/com/example/c/t24_fragment2/MainActivity.java
+++ T24_fragment2/app/src/main/java/com/example/c/t24_fragment2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.c.t24_fragment2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fr = null;
+        switch (v.getId()){
+            case R.id.btnAdd:
+                if(fr == null){
+                    fr = new BlankFragment();
+                    FragmentTransaction tr = fm.beginTransaction();
+                    tr.add(R.id.frame, fr, "counter");
+                    tr.commit();
+                }
+                break;
+            case R.id.btnRemove:
+                break;
+            case R.id.btnReplace:
+                break;
+            case R.id.btnHide:
+                break;
+        }
     }
 }

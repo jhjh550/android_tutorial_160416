1eb5630db3d2e8f209290d3cd13f025502503bd5
jh park <jhjh550@gmail.com>
2016-04-30 14:04:05 +0900

    -fragment2

---
+++ T24_fragment2/app/src/main/java/com/example/c/t24_fragment2/BlankFragment.java
@@ -0,0 +1,33 @@
+package com.example.c.t24_fragment2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class BlankFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView textViewCounter = (TextView) view.findViewById(R.id.textViewCounter);
+        Button btnIncrease = (Button)view.findViewById(R.id.btnIncrease);
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = textViewCounter.getText().toString();
+                int value = Integer.parseInt(str);
+                value++;
+                textViewCounter.setText(""+value);
+            }
+        });
+        return view;
+    }
+
+}
